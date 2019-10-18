7f9136b7e70ed20976cc2e2989bdb87e4dc33e01
jinyounghoon <jin304802@naver.com>
2019-10-18 14:47:12 +0900

    2016180041-진영훈-drill8-1
    2016180041-진영훈-drill8-1-커밋 횟수-3
    pause 함수 구현

--- main_state.py
+++ main_state.py
@@ -69,7 +69,7 @@ def handle_events():
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.change_state(title_state)
-        elif event.type == SDL_KEYDOWN and event.key == 'p':
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
             game_framework.change_state(pause_state)
 
 
